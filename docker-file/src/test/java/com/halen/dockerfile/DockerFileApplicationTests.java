1e96b7762a36bef19528d587b63223523fbca1f5
Halen <911452497@qq.com>
2020-12-06 20:17:28 +0800

    init

---
+++ docker-file/src/test/java/com/halen/dockerfile/DockerFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halen.dockerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
